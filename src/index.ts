dc2820403bb5709a9f904af5917f0e8d766c4a62
MadeByZeAman28 <103140996+MadeByZeAman28@users.noreply.github.com>
2022-06-12 23:38:45 +0300

    Try to get Client IP Address

--- src/index.ts
+++ src/index.ts
@@ -43,9 +43,9 @@ async function handleRequest({ request, client }: FetchEvent) {
 
   if (url.pathname == '/json') {
     const clientGeo = client.geo;
-    const clientIP = client.ip;
+    const clientIP = client?.ip;
 
-    return new Response(JSON.stringify({ geo: clientGeo, ip: clientIP }, null, 2), {
+    return new Response(JSON.stringify({ ip: clientIP, geo: clientGeo }, null, 2), {
       status: 200,
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
