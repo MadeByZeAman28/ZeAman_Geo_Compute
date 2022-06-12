f917df981635997bc7ea98194dcb573084577c2a
MadeByZeAman28 <103140996+MadeByZeAman28@users.noreply.github.com>
2022-06-13 00:46:58 +0300

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,8 +3,13 @@ import { reply } from "worktop/response";
 import { HTML } from '../GeoHTML';
 
 export const IndexGet: Handler = async (request, context) => {
+  const fastly_region = fastly.env.get('FASTLY_REGION');
+  const fastly_pop = fastly.env.get('FASTLY_POP');
+
   const headers = {
-    "content-type": "text/html"
+    "content-type": "text/html",
+    'X-Region': fastly_region',
+    'X-Pop-Code': fastly_pop
   }
 
   const clientGeo = context?.client?.geo;
