e4f738b8c1d5502cdaeb2543764578e0157d0c36
MadeByZeAman28 <103140996+MadeByZeAman28@users.noreply.github.com>
2022-06-13 00:47:43 +0300

    Update geoJson.ts

--- src/routes/geoJson.ts
+++ src/routes/geoJson.ts
@@ -2,7 +2,16 @@ import { Handler } from "worktop";
 import { reply } from "worktop/response";
 
 export const geoJson: Handler = async (request, context) => {
+  const fastly_region = fastly.env.get('FASTLY_REGION');
+  const fastly_pop = fastly.env.get('FASTLY_POP');
+
+  const headers = {
+    "content-type": "text/html",
+    'X-Region': fastly_region',
+    'X-Pop-Code': fastly_pop
+  }
+
   const clientGeo = context?.client?.geo;
 
-  return reply(200, clientGeo);
+  return reply(200, clientGeo, headers);
 }
